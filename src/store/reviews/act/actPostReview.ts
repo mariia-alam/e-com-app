da204a04897e199bae03de2cff52d6c0db8b8df8
mariia-alam <miaalam.91@yahoo.com>
2025-03-29 20:35:47 +0300

    connected feedback section to json server

--- src/store/reviews/act/actPostReview.ts
+++ src/store/reviews/act/actPostReview.ts
@@ -2,20 +2,17 @@ import { TReview } from "@customtypes";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import {AxiosErrorHandler} from "@util";
-import { RootState } from "@store/index";
 
 
 const actPostReview = createAsyncThunk("reviews/actPostReview",
     async(review: TReview,thunkAPI)=>{
-        const {rejectWithValue, getState} = thunkAPI;
-        const {auth} =  getState() as RootState;
+        const {rejectWithValue} = thunkAPI;
 
         try{
                 const response  = await axios.post("/reviews",review);
                 return response.data;
         }catch(error){
             return rejectWithValue(AxiosErrorHandler(error));
-
         }
 })
 
